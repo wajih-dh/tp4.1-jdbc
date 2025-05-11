c47fb6a064279bd9839e796e9356ed4e6b554d29
wajih-dh <wajihdhifli27@gmail.com>
2025-05-11 13:27:29 +0200

    final version

--- src/JDBCprojet/Test.java
+++ src/JDBCprojet/Test.java
@@ -4,33 +4,55 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-       Client c1 = new Client("ben mohamed","amine");
-       clientService cs = new clientService();
-       cs.create(c1);
-       System.out.println("client created "+c1.toString());
-       List<Client> clients = cs.findAll();
-            System.out.println("All clients:");
-            for (Client client : clients) {
-                System.out.println(client.toString());
-            }
+        clientService cs = new clientService();
 
-                        Client found = cs.findById(c1.getId());
-                        System.out.println("Found client by ID: " + found);
+        Client c1 = new Client("Ben Ali", "Ahmed");
+        Client c2 = new Client("Trabelsi", "Sami");
+        Client c3 = new Client("Kefi", "Rim");
+        Client c4 = new Client("Mansour", "Yasmine");
+        Client c5 = new Client("Saidi", "Nader");
 
-                        c1.setNom("updatedName");
-                        c1.setPrenom("updatedPrenom");
-                        cs.update(c1);
-                        Client updated = cs.findById(c1.getId());
-                        System.out.println("Updated client: " + updated);
+        cs.create(c1);
+        cs.create(c2);
+        cs.create(c3);
+        cs.create(c4);
+        cs.create(c5);
+        System.out.println(" 5 clients created.");
 
-                        cs.delete(c1);
-                        Client deleted = cs.findById(c1.getId());
-                        if (deleted == null) {
-                            System.out.println("Client deleted successfully.");
-                        } else {
-                            System.out.println("Client still exists: " + deleted);
-                        }
-          
-                    }
-                }
-    
+
+
+        Client client3 = cs.findById(3);
+        if (client3 != null) {
+            System.out.println(" Client with ID 3: " + client3);
+        } else {
+            System.out.println(" Client with ID 3 not found.");
+        }
+
+
+  
+        if (client3 != null) {
+            cs.delete(client3);
+            System.out.println(" Client with ID 3 deleted.");
+        }
+
+
+
+        Client client2 = cs.findById(2);
+        if (client2 != null) {
+            client2.setNom("ModifiedNom");
+            client2.setPrenom("ModifiedPrenom");
+            cs.update(client2);
+            System.out.println(" Client with ID 2 updated: " + client2);
+        } else {
+            System.out.println(" Client with ID 2 not found.");
+        }
+
+
+        
+        List<Client> clients = cs.findAll();
+        System.out.println(" Liste des clients:");
+        for (Client client : clients) {
+            System.out.println(client);
+        }
+    }
+}
